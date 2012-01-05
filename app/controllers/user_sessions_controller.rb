db41a71a79f4342ba16b42f286cf5e0f31d53504
tzumby <tzumby@gmail.com>
2012-01-05 15:18:12 -0500

    Added accounts controller and views

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      redirect_to root_url
+      redirect_to accounts_url
     else
       render :action => :new
     end
@@ -15,7 +15,7 @@ class UserSessionsController < ApplicationController
   
   def destroy
     current_user_session.destroy
-    redirect_to new_user_session_url
+    redirect_to root_url
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_filter :require_no_user, :only => [:new, :create]
+
   def new
     @user = User.new
   end
