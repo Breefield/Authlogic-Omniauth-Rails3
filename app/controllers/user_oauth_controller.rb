c6eda375086586862e4a6cdcd036eae6a7b3b1bf
tzumby <tzumby@gmail.com>
2012-01-05 15:24:56 -0500

    Added user omniauth controller

---
+++ app/controllers/user_oauth_controller.rb
@@ -0,0 +1,21 @@
+class UserOauthController < ApplicationController
+  def create
+    @current_user = User.find_or_create_from_oauth(auth_hash)
+    if current_user
+      UserSession.create(current_user, true)
+      redirect_to root_url, :notice => "Logged in"
+    else
+      redirect_to root_url, :flash => {:error => "Not authorized"}
+    end
+  end
+  
+  def failure
+    redirect_to root_url, :flash => {:error => "Not authorized. #{params[:message]}"}
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
