188c72ce48c2975eb2545bd65788602f9be1c61a
tzumby <tzumby@gmail.com>
2012-01-05 16:35:28 -0500

    Added form builders

---
+++ app/form_builders/labeled_form_builder.rb
@@ -0,0 +1,18 @@
+class LabeledFormBuilder < ActionView::Helpers::FormBuilder
+  
+  delegate :content_tag, :tag, to: :@template
+  
+  def error_messages
+    if object.errors.full_messages.any?
+      content_tag(:div, :class => "error_messages alert-message error") do
+        content_tag(:a, "x", :class => 'close') +
+        content_tag(:h3, "Invalid Fields") +
+        content_tag(:ul, :class => "unstyled") do
+          object.errors.full_messages.map do |msg|
+            content_tag(:li, msg)
+          end.join.html_safe
+        end
+      end
+    end
+  end
+end
